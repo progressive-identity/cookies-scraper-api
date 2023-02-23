78d13ce410c024f07daef8ec0e7a4c053eefead3
Jabuf <jason.buffet@gmail.com>
2023-02-23 19:13:41 +0100

    config: add a condition so that puppeteer can work locally

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,12 @@
 const {join} = require('path');
+require('dotenv').config()
 
 /**
  * @type {import("puppeteer").Configuration}
  */
-module.exports = {
-    // Changes the cache location for Puppeteer.
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
+if (process.env.NODE_ENV !== 'development') {
+    module.exports = {
+        // Changes the cache location for Puppeteer.
+        cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+    };
+}
