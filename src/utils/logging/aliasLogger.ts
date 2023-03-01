0901f9e060073f3de2ea50ecddd67dc98ce0e0a5
Jabuf <jason.buffet@gmail.com>
2023-03-01 12:33:25 +0100

    logger: add information on the logger when building it

--- src/utils/logging/aliasLogger.ts
+++ src/utils/logging/aliasLogger.ts
@@ -5,18 +5,23 @@ import dotenv from 'dotenv'
 import packageJson from './../../../package.json'
 
 dotenv.config()
+const nodeEnv = process.env.NODE_ENV ?? 'development'
+const logLevel = process.env.LOG_LEVEL ?? 'info'
 
-export let aliasLogger: Logger
 /**
  * @see {@link https://getpino.io/#/}
  * @see {@link https://www.elastic.co/guide/en/ecs-logging/nodejs/current/pino.html#pino}
  * @see {@link https://betterstack.com/community/guides/logging/how-to-install-setup-and-use-pino-to-log-node-js-applications/}
  */
-if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'development') {
-  aliasLogger = pino(ecsFormat())
+export let aliasLogger: Logger
+if (nodeEnv !== 'test' && nodeEnv !== 'development') {
+  aliasLogger = pino(ecsFormat({ convertReqRes: true }))
 } else {
   aliasLogger = pino({
     name: packageJson.name,
-    level: process.env.LOG_LEVEL ?? 'info',
+    level: logLevel,
   })
 }
+aliasLogger.info(
+  `Logger started on ${nodeEnv} environment, level :${aliasLogger.level}`
+)
