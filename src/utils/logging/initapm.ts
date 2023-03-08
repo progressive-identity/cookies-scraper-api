245fbe1b85dcb4a4ecd57e6fe861d7a5cf1ff6fc
Jabuf <jason.buffet@gmail.com>
2023-03-08 14:37:40 +0100

    config: use a different strategy to obtain the name of the application

--- src/utils/logging/initapm.ts
+++ src/utils/logging/initapm.ts
@@ -1,6 +1,7 @@
 import apm from 'elastic-apm-node'
 import dotenv from 'dotenv'
-import packageJson from './../../../package.json'
+import appRootPath from 'app-root-path'
+import path from 'path'
 
 dotenv.config()
 
@@ -8,10 +9,10 @@ dotenv.config()
  * APM is started in its own module so that it can be imported on the top of the main file (index.ts).
  * https://www.elastic.co/guide/en/apm/agent/nodejs/current/starting-the-agent.html#start-option-separate-init-module
  */
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test') {
   apm.start({
     serverUrl: process.env.ELASTIC_APM_SERVER_URL,
     secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
-    serviceName: packageJson.name,
+    serviceName: path.basename(appRootPath.toString()),
   })
 }
